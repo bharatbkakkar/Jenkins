412bf7ec60f53dfdc260692cb5e26f76ee392726
bharatbkakkar <83847521+bharatbkakkar@users.noreply.github.com>
2021-06-14 18:06:15 -0700

    Update FirstJenkinsScript.groovy

--- JenkinsScripts/FirstJenkinsScript.groovy
+++ JenkinsScripts/FirstJenkinsScript.groovy
@@ -8,12 +8,12 @@ pipeline {
                 sh '''
                     set +x
                     cd ~
+		    cd /var/lib/jenkins/workspace/HAEA Operations/InfraAuto-Git
 		    pwd
-                    #cd /var/lib/jenkins/workspace/HAEA Operations/InfraAuto-Git/Ansible/TestFirst.yml
                     echo "Run ansible playbooks"
                     echo "Run as user:"
                     whoami
-                    ansible-playbook "./HAEA Operations/InfraAuto-Git/Ansible/FirstAnisible.yml"
+                    ansible-playbook "./Ansible/FirstAnisible.yml"
 				'''
             }
         }
